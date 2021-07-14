ffe9492f08da8d5ece7004ac91c8f4b7585e062d
Aldy Rambe <aldyrmbe@gmail.com>
2021-07-14 16:07:01 +0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2329a4b2a15d1bd59446a50146bf424a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '80353ea3-cbb1-4655-a80b-fd2ae17c2c01'
+};
